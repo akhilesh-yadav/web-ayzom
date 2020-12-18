252da92091c21f3b7552e157dd1a635c2aa32756
Akhilesh Yadav1 <akhilesh.yadav1@morningstar.com>
2020-12-18 21:04:50 +0530

    first commit

---
+++ web/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+import store from "../store";
+import { onMounted, computed,ref } from "vue";
+
+export default {
+  name: 'HelloWorld',
+  setup() {
+    let count = ref(0);
+    const countries = computed(() => store.state.countries);
+    onMounted(() => store.dispatch("load"));
+
+    const removeItem = (item) => store.commit("removeItem", item); 
+
+
+    return {
+      countries,
+      removeItem,
+      count
+    };
+  },
+};
+
+</script>
